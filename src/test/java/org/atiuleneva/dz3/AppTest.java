3f3aa85467f3e2c7f10606a88431cb119bdf153f
AnnaT <anna.tiuleneva@outlook.com>
2021-01-22 13:59:11 +0300

    DZ5
    Moved Selenium test in Junit5.
    Added 2 test-case:
    1) Test_AddToQuotationBook;
    2) Test_DeleteQuote.

--- src/test/java/org/atiuleneva/dz3/AppTest.java
+++ src/test/java/org/atiuleneva/dz3/AppTest.java
@@ -1,20 +1,28 @@
 package org.atiuleneva.dz3;
 
-import static org.junit.Assert.assertTrue;
+import org.atiuleneva.dz4.DZ4_JunitTest;
+import org.junit.jupiter.api.*;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
-import org.junit.Test;
+//import static org.junit.Assert.assertTrue;
+
+//import org.junit.Test;
 
 /**
  * Unit test for simple App.
  */
+
 public class AppTest 
 {
+    public static Logger logger = LoggerFactory.getLogger(AppTest.class);
     /**
      * Rigorous Test :-)
      */
     @Test
     public void shouldAnswerWithTrue()
     {
-        assertTrue( true );
+
+        Assertions.assertTrue(true);
     }
 }
